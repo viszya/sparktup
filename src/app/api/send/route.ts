acca8c93cc95c941e7840142267e9982a6d9074f
Viszy A <viszya@Viszys-MacBook-Pro.local>
2023-11-14 17:15:21 -0800

    (feat) added email template to resend email api

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,22 +1,25 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
+import VercelInviteUserEmail from '@/app/_components/email-template';
 
 const resend = new Resend('re_MFVDVjeC_MR9Yvzgi1PRoW7kwDDHRtCdq');
 
 export async function GET() {
     try {
         const { data, error } = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
+            from: 'Sparktup <onboarding@resend.dev>',
             to: ['xanthan.ex@gmail.com'],
-            subject: 'Hello world',
-            react: '<p>Congratsadsfasdfasdfasdfadsfadsffsdasdfasdfasdfasdfasdfsdfafdafdsadsfaadfs on sending your <strong>first email</strong>!</p>',
+            subject: 'Welcome to Sparktup! Here is your verification code.',
+            react: VercelInviteUserEmail(),
         });
         if (error) {
+            console.log(error);
             return NextResponse.json({ error });
         }
         return NextResponse.json(data);
     } catch (error) {
+        console.log(error);
         return NextResponse.json({ error });
     }
 }
