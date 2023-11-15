075032b225bb939fa7e0fb5561db0f159d5a39ff
Viszy A <xanthan.ex@gmail.com>
2023-11-14 17:25:28 -0800

    (fix) moved resend api key to env

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 import VercelInviteUserEmail from '@/app/_components/email-template';
 
-const resend = new Resend('re_MFVDVjeC_MR9Yvzgi1PRoW7kwDDHRtCdq');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function GET() {
     try {
