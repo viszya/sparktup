ad03f7c9ba5bda2198adab63d13b8b44074f125f
Viszy A <xanthan.ex@gmail.com>
2023-11-15 21:11:22 -0800

    (fix) updated form layouts

--- src/app/_components/onboarding/form-2.tsx
+++ src/app/_components/onboarding/form-2.tsx
@@ -62,7 +62,7 @@ export function ProfileForm2() {
 									What is your full name (first / last)?
 								</label>
 								<input
-									className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+									className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-red-300 focus:outline-none focus:ring-red-300 sm:text-sm"
 									placeholder="Jeff Bezos"
 									type="text"
 									value={name}
@@ -75,7 +75,7 @@ export function ProfileForm2() {
 									What is your professional email address?
 								</label>
 								<input
-									className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+									className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-red-300 focus:outline-none focus:ring-red-300 sm:text-sm"
 									placeholder="jeffbezos@example.com"
 									type="text"
 									value={email}
@@ -88,7 +88,7 @@ export function ProfileForm2() {
 									What is your profession or job title?
 								</label>
 								<input
-									className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+									className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-red-300 focus:outline-none focus:ring-red-300 sm:text-sm"
 									placeholder="Richest Person in the World"
 									type="text"
 									value={jobTitle}
@@ -102,7 +102,7 @@ export function ProfileForm2() {
 										Your Personal Statement / Profile Summary
 									</label>
 									<div className="mt-1">
-										<textarea className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+										<textarea className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-red-300 focus:outline-none focus:ring-red-300 sm:text-sm"
 											placeholder="What are you working on?"
 											rows={4}
 											value={statement}
--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -10,7 +10,7 @@ import { ProfileForm2 } from "@/app/_components/onboarding/form-2";
 
 export default function Onboarding() {
 	return (
-		<div className="flex flex-col justify-center items-center mt-20">
+		<div className="flex flex-col justify-center items-center mt-20 mb-10">
 			<Tabs defaultValue="account">
 				<TabsList className="gap-x-6 px-4">
 					<TabsTrigger value="account">Account</TabsTrigger>
