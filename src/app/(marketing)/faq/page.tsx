9e6312ce1f09ebd24989409d0fdec80e7647a717
Vedant <lvedant54@gmail.com>
2023-11-18 14:35:26 -0800

    (update) Updated FAQ

--- src/app/(marketing)/faq/page.tsx
+++ src/app/(marketing)/faq/page.tsx
@@ -11,24 +11,24 @@ import { cn } from "@/server/utils"
 export default function Page() {
     return (
         <div className={cn("flex flex-col justify-center items-center", twp().wrapper)}>
-            <h2 className="mt-20 sm:mt-30 mb-2 text-center text-4xl font-bold">FAQ</h2>
-            <Accordion type="single" collapsible className="text-left w-full max-w-sm md:max-w-lg lg:max-w-2xl h-[45rem] max-h-[45rem] md:h-[40rem] md:max-h-[40rem]">
+            <h2 className="mt-20 sm:mt-30 mb-11 text-center text-4xl font-bold">FAQ</h2>
+            <Accordion type="single" collapsible className="text-left w-full max-w-sm md:max-w-lg lg:max-w-2xl h-[55rem] max-h-[55rem] md:h-[55rem] md:max-h-[55rem]">
                 <AccordionItem value="item-1">
-                    <AccordionTrigger className="text-left">Is it accessible?</AccordionTrigger>
+                    <AccordionTrigger className="text-left">How do I get started with Sparktup?</AccordionTrigger>
                     <AccordionContent>
-                        Yes. It adheres to the WAI-ARIA design pattern.
+                        Simply visit our website and click on "Sign Up." Follow the prompts to create your account and unlock a world of tech opportunities.
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="item-2">
-                    <AccordionTrigger className="text-left">How does SparktUp use AI for business plans?</AccordionTrigger>
+                    <AccordionTrigger className="text-left">Can I edit my profile information after signing up?</AccordionTrigger>
                     <AccordionContent>
-                        SparktUp leverages artificial intelligence to analyze your project goals and data to generate comprehensive business plans tailored to your SAAS idea.
+                        Yes, you can easily edit your profile information by accessing your account settings. Keep your details up-to-date to enhance your Sparktup experience.
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="item-3">
-                    <AccordionTrigger className="text-left">What tools are provided for project management?</AccordionTrigger>
+                    <AccordionTrigger className="text-left">How do businesses post job listings on Sparktup?</AccordionTrigger>
                     <AccordionContent>
-                        SparktUp offers a suite of project management tools, including task tracking, marketing event tracking, financial analysis, and more to help streamline your SAAS project.
+                        Businesses can post job openings by logging into their accounts, navigating to the "Post a Job" section, and filling out the required details.
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="item-4">
@@ -40,7 +40,7 @@ export default function Page() {
                 <AccordionItem value="item-5">
                     <AccordionTrigger className="text-left">How quickly can I go from idea to a running SAAS product?</AccordionTrigger>
                     <AccordionContent>
-                        With SparktUp&apos;s efficient processes and guidance, you can transform your concept into a fully operational SAAS product in just one week.
+                        With SparktUp's efficient processes and guidance, you can transform your concept into a fully operational SAAS product in just one week.
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="item-6">
@@ -71,6 +71,36 @@ export default function Page() {
                     <AccordionTrigger className="text-left">Is SparktUp customizable to my specific project needs?</AccordionTrigger>
                     <AccordionContent>
                         Yes, SparktUp is designed to be flexible and can be customized to suit the unique requirements of your SAAS project.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-11">
+                    <AccordionTrigger className="text-left">Is there a community aspect to Sparktup?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes, Sparktup is a vibrant tech community. Engage in discussions, network with professionals, and stay updated on industry trends within our collaborative space.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-12">
+                    <AccordionTrigger className="text-left">How does Sparktup ensure the privacy and security of user information?</AccordionTrigger>
+                    <AccordionContent>
+                        We take privacy seriously. Our robust security measures protect your data, and our privacy policy outlines how information is collected, used, and stored.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-13">
+                    <AccordionTrigger className="text-left">Can I customize my notification preferences?</AccordionTrigger>
+                    <AccordionContent>
+                        Absolutely! You can manage your notification settings in your account to receive updates tailored to your preferences.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-14">
+                    <AccordionTrigger className="text-left">Can I showcase specific tech skills on my Sparktup profile?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes, you can highlight your tech skills on your profile, making it easier for businesses to find candidates with the expertise they need.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-15">
+                    <AccordionTrigger className="text-left">How can I provide feedback or get support on Sparktup?</AccordionTrigger>
+                    <AccordionContent>
+                        We value your feedback! Reach out to our support team through the "Contact Us" section on the website, and we'll assist you promptly.
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
