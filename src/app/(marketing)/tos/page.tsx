1340f9b1cd86fb1d50b3b7874bef88107ad9d395
OverClockedGamer <dearakhi@gmail.com>
2023-11-18 17:09:39 -0800

    Updated TOS to make sure its correct

--- src/app/(marketing)/tos/page.tsx
+++ src/app/(marketing)/tos/page.tsx
@@ -1,89 +1,172 @@
 import { cn } from "@/server/utils";
 import { twp } from "@/server/utils";
 
-export default function AboutUs() {
+export default function TermsOfService() {
   return (
     <div className={cn(twp().wrapper, "mt-20 mx-20")}>
       <article>
         <div className="text-left mt-10 text-3xl tracking-tighter text-black">
-          About Sparktup
+          Terms of Service for Sparktup
         </div>
         <div className="mt-4 text-gray-500 text-lg">
           <p>
-            Welcome to Sparktup, where innovation meets opportunity. Founded by
-            Vedant Lad, Vishwa Anand, and Akshay Burra, Sparktup was born out of
-            a shared vision to revolutionize the way individuals and businesses
-            connect in the dynamic world of technology.
-          </p>
-        </div>
-      </article>
-      <article>
-        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
-          Company Description:
-        </div>
-        <div className="mt-4 text-gray-500 text-lg">
-          <p>
-            At Sparktup, we've created more than just a platform; we've built a
-            dynamic ecosystem where tech enthusiasts, professionals, and
-            businesses converge. Our platform serves as a catalyst, sparking
-            connections that drive innovation and propel careers.
-          </p>
-        </div>
-      </article>
-      <article>
-        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
-          Mission and Goals:
-        </div>
-        <div className="mt-4 text-gray-500 text-lg">
-          <p>
-            <strong>Our Mission:</strong>
+            <strong>1. Acceptance of Terms:</strong>
             <br />
-            To connect businesses with top-tier tech talent while empowering
-            individuals to ignite and advance their tech careers.
-          </p>
-          <p>
-            <strong>Our Goals:</strong>
-            <br />
-            <ol>
-              <li>
-                <strong>Empowerment:</strong> Empower individuals to take
-                control of their tech journeys by providing access to exciting
-                opportunities and resources.
-              </li>
-              <li>
-                <strong>Innovation:</strong> Foster a vibrant tech ecosystem
-                that encourages collaboration, innovation, and continuous
-                learning.
-              </li>
-              <li>
-                <strong>Simplicity:</strong> Streamline the process for
-                businesses to find the right talent and for individuals to
-                discover their ideal tech opportunities with ease.
-              </li>
-              <li>
-                <strong>Community:</strong> Cultivate a supportive community
-                where success is shared, and knowledge is a driving force.
-              </li>
-            </ol>
+            By accessing or using the services provided by Sparktup, you agree
+            to comply with and be bound by these Terms of Service. If you do
+            not agree with these Terms, please refrain from using our services.
           </p>
         </div>
       </article>
       <article>
         <div className="text-left mt-20 text-3xl tracking-tighter text-black">
-          Join Us in Sparking Tech Success:
+          2. User Eligibility
         </div>
         <div className="mt-4 text-gray-500 text-lg">
           <p>
-            Whether you're a business seeking top tech talent or an individual
-            ready to ignite your tech career, Sparktup is your go-to platform.
-            Join us in shaping the future of technology—where connections lead
-            to innovation and opportunities spark growth.
+            <strong>a. Account Creation:</strong>
+            <br />
+            Users are required to create an account to access certain features
+            of Sparktup.
           </p>
           <p>
-            <em>Sparktup - Where Tech Dreams Take Flight.</em>
+            <strong>b. Account Information:</strong>
+            <br />
+            You are responsible for providing accurate and updated information
+            during account creation.
+          </p>
+          <p>
+            <strong>c. Account Security:</strong>
+            <br />
+            Keep your account credentials secure. Notify us immediately of any
+            unauthorized use.
           </p>
         </div>
       </article>
+      <article>
+        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
+          3. Use of Services
+        </div>
+        <div className="mt-4 text-gray-500 text-lg">
+          <p>
+            <strong>a. Lawful Use:</strong>
+            <br />
+            Users agree to use Sparktup for lawful purposes only, complying
+            with all applicable laws and regulations.
+          </p>
+          <p>
+            <strong>b. Prohibited Activities:</strong>
+            <br />
+            Users must not engage in activities that violate these Terms,
+            infringe on intellectual property rights, or harm the platform's
+            functionality.
+          </p>
+        </div>
+      </article>
+
+      <article>
+        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
+          4. Privacy
+        </div>
+        <div className="mt-4 text-gray-500 text-lg">
+          <p>
+            <strong>Our Privacy Policy governs the collection, use, and disclosure of user information. By using Sparktup, you consent to our privacy practices outlined in the Privacy Policy.</strong>
+          </p>
+        </div>
+      </article>
+
+      <article>
+        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
+          5. Intellectual Property
+        </div>
+        <div className="mt-4 text-gray-500 text-lg">
+          <p>
+            <strong>a. Ownership:</strong>
+            <br />
+            Sparktup retains ownership of all intellectual property associated with the platform.
+          </p>
+          <p>
+            <strong>b. User Content:</strong>
+            <br />
+            By submitting content to Sparktup, users grant us a non-exclusive, worldwide, royalty-free license to use, reproduce, and distribute the content.
+          </p>
+        </div>
+      </article>
+
+      <article>
+        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
+          6. Termination
+        </div>
+        <div className="mt-4 text-gray-500 text-lg">
+          <p>
+            <strong>We reserve the right to terminate or suspend user accounts for violations of these Terms or for any reason at our discretion.</strong>
+          </p>
+        </div>
+      </article>
+
+      <article>
+        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
+          7. Disclaimer of Warranties
+        </div>
+        <div className="mt-4 text-gray-500 text-lg">
+          <p>
+            <strong>Sparktup is provided "as is" and "as available." We make no warranties, express or implied, regarding the platform's reliability, accuracy, or availability.</strong>
+          </p>
+        </div>
+      </article>
+
+      <article>
+        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
+          8. Limitation of Liability
+        </div>
+        <div className="mt-4 text-gray-500 text-lg">
+          <p>
+            <strong>We are not liable for any direct, indirect, incidental, special, or consequential damages resulting from your use of Sparktup.</strong>
+          </p>
+        </div>
+      </article>
+
+      <article>
+        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
+          9. Governing Law
+        </div>
+        <div className="mt-4 text-gray-500 text-lg">
+          <p>
+            <strong>These Terms are governed by the laws of the United States. Any legal action or proceeding related to Sparktup shall be brought exclusively in the courts of Washington.</strong>
+          </p>
+        </div>
+      </article>
+
+      <article>
+        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
+          10. Changes to Terms
+        </div>
+        <div className="mt-4 text-gray-500 text-lg">
+          <p>
+            <strong>We reserve the right to modify or revise these Terms at any time. Users will be notified of material changes, and continued use of Sparktup indicates acceptance of the updated Terms.</strong>
+          </p>
+        </div>
+      </article>
+
+      <article>
+        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
+          11. Contact Information
+        </div>
+        <div className="mt-4 text-gray-500 text-lg">
+          <p>
+            <strong>For any questions or concerns related to these Terms of Service, please contact us at sparktupfbla@gmail.com.</strong>
+          </p>
+        </div>
+      </article>
+
+      <article>
+        <div className="text-left mt-20 text-3xl tracking-tighter text-black">
+          These Terms of Service are effective as of 12/31/2024. Continued use of Sparktup signifies acceptance of these terms.
+        </div>
+      </article>
+
+
+
     </div>
   );
 }
