35a17b6a8cd0b63096f08b7c80a76d8640c78041
Vedant <lvedant54@gmail.com>
2024-01-01 19:23:37 -0800

    (update) Mobile Complient Dashboard Settings

--- src/app/_components/dashboard/settings.tsx
+++ src/app/_components/dashboard/settings.tsx
@@ -1,29 +1,32 @@
-import { Icons } from "@/app/_components/icons"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/app/_components/ui/tabs"
+import { Icons } from "@/app/_components/icons";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/app/_components/ui/tabs";
 import { Form1 } from "@/app/_components/applicant-settings/form-1";
 import { Form2 } from "@/app/_components/applicant-settings/form-2";
-import { InputData }  from "@/app/_components/inputdata"
+import { InputData } from "@/app/_components/inputdata";
 
 export function Settings() {
-    return (
-        <div className="flex flex-col justify-center items-center ">
-            <div className="mt-3 text-3xl tracking-tighter text-black">
-                Settings
-            </div>
-            <div className="flex flex-col justify-center w-full max-w-5xl">
-                <Tabs defaultValue="account" className="flex flex-col justify-center mt-5">
-                    <TabsList className="gap-x-6 px-4 ">
-                        <TabsTrigger value="account">Account</TabsTrigger>
-                        <Icons.chevronRight className="text-gray-300 h-5 w-5" />
-                        <TabsTrigger value="skills">Skills</TabsTrigger>
-                    </TabsList>
-                    <div className="w-full">
-                        <TabsContent value="account"><Form1 /></TabsContent>
-                        <TabsContent value="skills"><InputData /></TabsContent>
-                    </div>
-                </Tabs>
-            </div>
-
-        </div>
-    )
+  return (
+    <div className="flex flex-col justify-center items-center p-4">
+      <div className="mt-3 text-3xl tracking-tighter text-black">
+        Settings
+      </div>
+      <div className="flex flex-col justify-center w-full max-w-5xl">
+        <Tabs defaultValue="account" className="flex flex-col justify-center mt-5">
+          <TabsList className="flex flex-row gap-x-6 px-4 overflow-x-auto">
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <Icons.chevronRight className="text-gray-300 h-5 w-5" />
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+          </TabsList>
+          <div className="w-full">
+            <TabsContent value="account">
+              <Form1 />
+            </TabsContent>
+            <TabsContent value="skills">
+              <InputData />
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </div>
+  );
 }
