c9c328fc04e761ee41425caeffdcf9617262b151
viszya <xanthan.ex@gmail.com>
2024-02-02 11:54:36 -0800

    favicon

--- src/app/_config/site.ts
+++ src/app/_config/site.ts
@@ -3,7 +3,7 @@ import type { SiteConfig } from "@/server/types"
 export const siteConfig: SiteConfig = {
   name: "Sparktup",
   description:
-    "SparktUp is a web app that helps solo developers and small teams to build and grow their products.",
+    "SparktUp is a web app that helps companies connect with their applicants in the most efficient way possible.",
   url: "",
   ogImage: "",
 }
